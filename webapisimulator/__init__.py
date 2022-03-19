968de2ca731e20df679dfe9eaf97cc2cb23ddc70
Mikhail Eremeev <meremeev@sfedu.ru>
2022-03-19 11:00:43 +0300

    build: add version

---
+++ webapisimulator/__init__.py
@@ -0,0 +1,6 @@
+from .utils import get_version_number
+
+
+VERSION = (0, 1, 0)
+
+__version__ = get_version_number(VERSION)
---
+++ webapisimulator/utils.py
@@ -0,0 +1,4 @@
+
+def get_version_number(version):
+    version_number = '.'.join((str(number) for number in version))
+    return version_number
