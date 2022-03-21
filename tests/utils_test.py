217f072c6835a96d027c14c28c129c368caeca07
Mikhail Eremeev <meremeev@sfedu.ru>
2022-03-21 12:03:03 +0300

    test: add utils test case

---
+++ tests/utils_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+from webapisimulator import utils
+
+
+class UtilsTestCase(unittest.TestCase):
+    def test_should_get_version_number(self):
+        expected = '1.0.0'
+        actual = utils.get_version_number((1, 0, 0))
+        self.assertEqual(expected, actual)
