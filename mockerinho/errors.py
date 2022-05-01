ede9fd230def4f1338aaa2ada704db719f856c42
Mikhail Eremeev <meremeev@sfedu.ru>
2022-05-01 23:19:38 +0300

    refactor: rename Error -> MockerinhoError

--- mockerinho/errors.py
+++ mockerinho/errors.py
@@ -1,11 +1,12 @@
 
-
-class Error(Exception):
+class MockerinhoError(Exception):
+    """A base class for errors used by mockerinho."""
     pass
 
 
-class MatcherError(Error):
-    pass
+class MatcherError(MockerinhoError):
+    """This is a base class for all errors related
+    to request matchers."""
 
 
 class IncorrectSuitableError(MatcherError):
