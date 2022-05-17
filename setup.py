8fb7ad02c56eeba2a6a8b305fc6727faca17db6a
Mikhail Eremeev <meremeev@sfedu.ru>
2022-05-17 15:18:42 +0300

    build: add setup script
    Closes #3

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+* Install with pip (recommended):
+    pip3 install .
+* Install with setuptools:
+    python3 setup.py install
+"""
+from setuptools import setup, find_packages
+
+from mockerinho import __version__
+
+
+def get_install_requires(requirements_filename: str) -> 'list[str]':
+    install_requires = []
+    with open(requirements_filename) as f:
+        for requirement in f.readlines():
+            install_requires.append(requirement)
+    return install_requires
+
+
+setup(
+    name='mockerinho',
+    description='Lightweight tool designed to simulate web API endpoints.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    version=__version__,
+    url='https://github.com/callmecoolmanx/mockerinho',
+    author='Mikhail Eremeev',
+    author_email='meremeev@sfedu.ru',
+    license='MIT',
+    platforms=('any',),
+    packages=find_packages(exclude=('tests',)),
+    python_requires='>=3.7,<=3.10',
+    install_requires=get_install_requires('requirements.txt'),
+)
